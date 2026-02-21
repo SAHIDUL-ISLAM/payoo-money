5c1873cf44b50b2018025c1fb4e454cb5e9c04c0
SAHIDUL-ISLAM <sahidul99622@gmail.com>
2026-02-21 09:47:01 +0600

    login page validation

--- script/login.js
+++ script/login.js
@@ -1,1 +1,17 @@
-console.log("Ok");
+document.getElementById('login-btn').addEventListener('click',function(){
+    const numberCheck = document.getElementById('number');
+    const numberVlaue = numberCheck.value;
+    const pinCheck = document.getElementById('pin');
+    const pinValue = pinCheck.value;
+    
+    const validNum = 1785438758;
+    const validPin = 1234;
+    if(numberVlaue == validNum && pinValue == validPin){
+        alert("Longin Success");
+        window.location.assign("/home.html");
+    }else{
+        alert("Longin Failed");
+        return;
+    }
+
+})
