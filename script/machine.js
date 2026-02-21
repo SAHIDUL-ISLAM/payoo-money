5592306ee34b780add84933dab884c78ddcf41d6
SAHIDUL-ISLAM <sahidul99622@gmail.com>
2026-02-21 15:31:35 +0600

    add money and cashout button validate

--- script/machine.js
+++ script/machine.js
@@ -20,3 +20,15 @@ function setBalance(value){
     balanceElement.innerText = value;
 }
 
+//machine > hide all > show what you need
+function showOnly(id){
+    const addMoney = document.getElementById("add-money");
+    const cashout = document.getElementById("cashout");
+
+    addMoney.classList.add("hidden");
+    cashout.classList.add("hidden");
+
+    // show what you click 
+    const seleted = document.getElementById(id);
+    seleted.classList.remove("hidden")
+}
